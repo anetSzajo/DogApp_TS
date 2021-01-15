0394de1f61dc5701f96be7c6123f8f5850ab6293
Aneta Szajowska <an.szajowska@gmail.com>
2021-01-15 17:30:27 +0100

    Extract transportApiResponse function to utils.

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,3 +1,11 @@
+import {BreedApiResponse} from "./Model/BreedApiResponse";
+import {Breed} from "./Model/Breed";
+
 export function capitalizeFirstLetter(word: string): string {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+export const transformApiResponse = ((data: BreedApiResponse): Breed[] =>
+    Object.entries(data.message).map((n) => {
+        return {breedName: n[0], subBreeds: n[1]};
+    }))
