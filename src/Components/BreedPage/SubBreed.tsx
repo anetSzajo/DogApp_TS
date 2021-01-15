96110c80eb5798f0cd177e47858f6a59605c58ed
Aneta Szajowska <an.szajowska@gmail.com>
2021-01-15 13:46:21 +0100

    Implement SubBreed component.

---
+++ src/Components/BreedPage/SubBreed.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {capitalizeFirstLetter} from '../../utils';
+// import '../../../main.scss';
+
+export function SubBreed(props: {onClick: Function, subBreedName: string }) {
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>,) => {
+        props.onClick(e.currentTarget.value)
+    };
+
+    return (
+        <div className="subBreedsList__element">
+            <button onClick={handleClick}>{capitalizeFirstLetter(props.subBreedName)}</button>
+        </div>)
+}
