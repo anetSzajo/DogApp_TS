1f6073534a4f3745b18af42f7a0fcd51ea43e277
Aneta Szajowska <an.szajowska@gmail.com>
2021-01-15 17:31:23 +0100

    Refactor app component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import BreedPage from "./Components/BreedPage/BreedPage";
+import HomePage from "./Components/HomePage";
+import Footer from "./Components/Footer";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+        <Router>
+          <Navbar/>
+          <Switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route exact path="/:breedKey" component={BreedPage}/>
+          </Switch>
+        </Router>
+        <Footer/>
+      </div>
   );
 }
 
-export default App;
